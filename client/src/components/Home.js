898ac77e173fac054095ff51ac67cc110150ff7a
Ian Harwell <sirharwell@gmail.com>
2018-06-02 13:16:01 -0600

    attempted fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -18,8 +18,8 @@ class Home extends React.Component {
   render(){
     return(
       <div>
-      <Segment inverted fluid textAlign='center' style={{
-        backgroundImage: `url('./photos/Brugges.jpg')`, backgroundSize: " cover ", minHeight: 700, padding: '1em 0em' }} vertical
+      <Segment inverted textAlign='center' style={{
+        backgroundImage: `url(https://lh3.googleusercontent.com/sw2TzwDt3LP5jmrXhhqgqpXPiFY2B7yww5t0Pji8gSfnzBcyHXIlimM2xvYRVLAuHhOGMborkA7TyFCNIT6VUn4luoLN1er91Up7bhWm7rp0KmPvhUCeHZ6yprb1LYBnns3arlBtYxJ6a5LryIA0PneqflQEPhTIF6DRlGcjGUEN-ard7ZvXcjrjtVO5dYC570AcE0mL-eT7flNe3hYVfHi7FBR8lMKEP0SOn5A4rqqqyu1d6IJOtwk-rcpX7fb63WooqDNk61WitPsyk9eKcx3BbGx5CzFnnEjfyV8kgEOHfkTA7U66fA96H6o9zqby0qso3A09mNQ8zmg4L_3gApJ6m_SSuVZmVPep3DlBh-hEc2TUrGhIi2sr5XTr7OBztOe_9JudZyLZtRe-gNbfLg9_5G2tyiGiHSvzzc_VfJG3UjAbaoDfzdOb-KONtHex4MFKD8QC-E7zj8cyDGuDd3LP9UoEGVltau0cEy7TvCtfUBxx0tTMHLuEofWd0T3PodT4KEn_dwZjLi1XaaSVNsQQMHOwN9d5ZgiJ60gXCn72PWu7l-SVNDCGXYzU9qUiW_90DKfhlXKdiEdDkceV2dastAlYVtasVsolepHX=w1239-h697-no)`, backgroundSize: " cover ", minHeight: 700, padding: '1em 0em' }} vertical
       >
         <Container text style={{marginTop:'30px'}}>
           <Segment style={{ backgroundColor: 'rgba(250, 250, 250, .0)', padding: '20px'}}>
@@ -38,14 +38,14 @@ class Home extends React.Component {
               <Header as='h3' style={{ fontSize: '2em' }}>I have been married for 8 years</Header>
               <p style={{ fontSize: '1.33em' }}>
                 She's the super coolest, you can learn more about her
-              <a href='/family'>here.</a></p>
+              </p><a href=''>here.</a>
             </Grid.Column>
             <Grid.Column floated='right' width={6}>
               <Image
                 bordered
                 rounded
                 size='large'
-                src='./photos/Arches.jpg'
+                src='https://lh3.googleusercontent.com/NvP-49W-uHr6vPqDdb7IvHrwfoMVzSCKPuY-CwfM5IODhl0S4GTDfnxN9jyTNL7cNzQott1n2n1pDz0LuCEkTARHyXY-z74fWoOdRvLTQJis7ZHozqQxxQpBxJiwsZTpH7aeKpgwjw6Izf4oSIjTprujcngGOQ76fBUptSV9xmy4lEcJbXRabthKztKUQ2RWT9wFycF2ZCNwKhQLq0d0ZNtMAs_v4GN7GLBB47LkeJ3Y_qgEugGt9uICCZnJYHGhnCXqscKMaDtDVFTccsCIimW1La-LHlgGODfoU0KNAtB_34tEC0NbELltwGnsnjG5Q9NGtGLF4_JFj1pE6J3A872_1i5kCUTZCUtIaM9OK80IYHNwtnTlLTOFcybVuzpDxnPrOlMfKCEfPcwP9BI_bLe-fBkLOnu3uN1tpzpO6ERawbLOFYgNTN-GCo-I6fAliOyIS_DErgZe9TIoui3ZdTFJ3_GmREIQI1r7X5std_qYIdb1-hTt9X2DHvhlfLUsSkwb5_YrdOPgYgk4XILjxR2AXOdWBqXQso4GfMHQntJQWi_joszAWT2R0zZrHyQ3FkuTCtimEB1CzCr-Rc36SdSy50532QAO4YZyF3Wk=w570-h759-no'
               />
             </Grid.Column>
           </Grid.Row>
@@ -73,7 +73,7 @@ class Home extends React.Component {
             I like to travel. I enjoy going camping and jeeping. I want to try and visit all the wonders of the world.
             <br/>
             <br/>
-            If you want to know more about me, feel free to explore the website. If that doesn't seem like quite enough, hit me up on social media or just send me a text/email like the good ol' days. I typically get online at least once a day even when out exploring the world, so response time shouldn't be too long.
+            If you want to know more about me, feel free to explore the website. If that doesn't sem like quite enough, hit me up on social media or just send me a text/email like the gold ol' days. I typically get online at least once a day even when out exploring the world, so response time shouldn't be too long.
           </p>
           <Divider
             as='h4'
